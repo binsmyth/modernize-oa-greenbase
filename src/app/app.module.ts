485429d25549a3ab3604302c1e6d22e31a074fbe
vineet man singh <binsmyth@gmail.com>
2024-02-06 14:59:57 +1100

    added top bar component, adjust some naming of elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SettingsComponent } from './settings/settings.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ContentSectionComponent } from './content-section/content-section.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, TopbarComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
